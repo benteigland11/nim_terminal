e89200de80d7eeb6c50db8df7009d34fe5d6c074
benteigland11 <benteigland11@gmail.com>
2026-04-25 16:16:29 -0500

    Fix Windows FFI: Handle WideCString nil assignment

--- src/pty/windows_backend.nim
+++ src/pty/windows_backend.nim
@@ -1,7 +1,9 @@
 ## Windows implementation of Pseudo-terminal (ConPTY).
 
 when defined(windows):
-  import std/winlean
+  import std/[winlean, os]
+  import ../../cg/universal_windows_handle_nim/src/windows_handle_lib
+  import ../../cg/universal_windows_error_nim/src/windows_error_lib
 
   type
     HPCON* = Handle
@@ -9,25 +11,147 @@ when defined(windows):
       x*, y*: int16
     HRESULT* = int32
     DWORD = uint32
+    SIZE_T = uint
 
   proc CreatePseudoConsole*(size: COORD, hInput, hOutput: Handle, flags: DWORD, phPC: ptr HPCON): HRESULT {.stdcall, importc: "CreatePseudoConsole", dynlib: "kernel32.dll".}
   proc ResizePseudoConsole*(hPC: HPCON, size: COORD): HRESULT {.stdcall, importc: "ResizePseudoConsole", dynlib: "kernel32.dll".}
   proc ClosePseudoConsole*(hPC: HPCON) {.stdcall, importc: "ClosePseudoConsole", dynlib: "kernel32.dll".}
 
+  # For Process Creation
+  type
+    STARTUPINFOEXW = object
+      StartupInfo: STARTUPINFO
+      lpAttributeList: pointer
+
+  proc InitializeProcThreadAttributeList*(lpAttributeList: pointer, dwAttributeCount: DWORD, dwFlags: DWORD, lpSize: ptr SIZE_T): WINBOOL {.stdcall, importc: "InitializeProcThreadAttributeList", dynlib: "kernel32.dll".}
+  proc UpdateProcThreadAttribute*(lpAttributeList: pointer, dwFlags: DWORD, Attribute: SIZE_T, lpValue: pointer, cbSize: SIZE_T, lpPreviousValue: pointer, lpReturnSize: ptr SIZE_T): WINBOOL {.stdcall, importc: "UpdateProcThreadAttribute", dynlib: "kernel32.dll".}
+  proc DeleteProcThreadAttributeList*(lpAttributeList: pointer) {.stdcall, importc: "DeleteProcThreadAttributeList", dynlib: "kernel32.dll".}
+
+  const
+    EXTENDED_STARTUPINFO_PRESENT = 0x00080000'i32
+    PROC_THREAD_ATTRIBUTE_PSEUDOCONSOLE = 0x00020016'u
+
   type
     WindowsBackend* = ref object
       hPC*: HPCON
+      ptyIn*: SafeHandle
+      ptyOut*: SafeHandle
+      processHandle*: SafeHandle
+      threadHandle*: SafeHandle
+      pid*: int
 
   proc newWindowsBackend*(): WindowsBackend = WindowsBackend()
 
-  proc ptyOpen*(b: WindowsBackend): tuple[handle: int, slaveId: string] = (0, "")
-  proc ptyRead*(b: WindowsBackend, h: int, buf: var openArray[byte]): int = 0
-  proc ptyWrite*(b: WindowsBackend, h: int, data: openArray[byte]): int = 0
-  proc ptyResize*(b: WindowsBackend, h: int, rows, cols: int) = discard
-  proc ptySignal*(b: WindowsBackend, p, s: int) = discard
-  proc ptyWait*(b: WindowsBackend, p: int): int = 0
-  proc ptyClose*(b: WindowsBackend, h: int) = discard
+  proc closeHandleCloser(h: int) =
+    discard closeHandle(Handle(h))
 
-  # Mandatory Host integration stubs
-  proc ptySetSize*(b: WindowsBackend, h, rows, cols: int) = discard
-  proc ptyForkExec*(b: WindowsBackend, h: string, p: string, a: openArray[string], c: string): int = 0
+  proc createPipePair(): (SafeHandle, SafeHandle) =
+    var readPipe, writePipe: Handle
+    var sa: SECURITY_ATTRIBUTES
+    sa.nLength = int32(sizeof(SECURITY_ATTRIBUTES))
+    sa.bInheritHandle = 0 # Cannot inherit directly for PTY
+    sa.lpSecurityDescriptor = nil
+    if createPipe(readPipe, writePipe, sa, 0) == 0:
+      raiseWinError(osLastError().int64, "CreatePipe")
+    (wrap(int(readPipe), closeHandleCloser), wrap(int(writePipe), closeHandleCloser))
+
+  proc ptyOpen*(b: WindowsBackend): tuple[handle: int, slaveId: string] =
+    var (outRead, outWrite) = createPipePair()
+    var (inRead, inWrite) = createPipePair()
+
+    var size = COORD(x: 80, y: 24)
+    var hPC: HPCON
+    let hr = CreatePseudoConsole(size, Handle(inRead.value), Handle(outWrite.value), 0, addr hPC)
+    if hr < 0:
+      raiseWinError(hr, "CreatePseudoConsole")
+
+    b.hPC = hPC
+    b.ptyIn = inWrite
+    b.ptyOut = outRead
+    
+    inRead.close()
+    outWrite.close()
+
+    (1, "conpty")
+
+  proc ptyRead*(b: WindowsBackend, h: int, buf: var openArray[byte]): int =
+    var bytesRead: int32
+    if readFile(Handle(b.ptyOut.value), addr buf[0], int32(buf.len), addr bytesRead, nil) == 0:
+      let err = osLastError().int64
+      if err == 109: return -1 # ERROR_BROKEN_PIPE
+      return -1
+    int(bytesRead)
+
+  proc ptyWrite*(b: WindowsBackend, h: int, data: openArray[byte]): int =
+    if data.len == 0: return 0
+    var bytesWritten: int32
+    if writeFile(Handle(b.ptyIn.value), unsafeAddr data[0], int32(data.len), addr bytesWritten, nil) == 0:
+      return -1
+    int(bytesWritten)
+
+  proc ptyResize*(b: WindowsBackend, h: int, rows, cols: int) =
+    var size = COORD(x: int16(cols), y: int16(rows))
+    discard ResizePseudoConsole(b.hPC, size)
+
+  proc ptySignal*(b: WindowsBackend, p, s: int) =
+    discard terminateProcess(Handle(b.processHandle.value), 1)
+
+  proc ptyWait*(b: WindowsBackend, p: int): int =
+    let res = waitForSingleObject(Handle(b.processHandle.value), 0)
+    if res == 0: # WAIT_OBJECT_0
+      var exitCode: int32
+      if getExitCodeProcess(Handle(b.processHandle.value), exitCode) != 0:
+        return int(exitCode)
+      return 0
+    return -1
+
+  proc ptyClose*(b: WindowsBackend, h: int) =
+    b.ptyIn.close()
+    b.ptyOut.close()
+    if b.hPC != 0:
+      ClosePseudoConsole(b.hPC)
+      b.hPC = 0
+    b.processHandle.close()
+    b.threadHandle.close()
+
+  # Host integration
+  proc ptySetSize*(b: WindowsBackend, h, rows, cols: int) =
+    ptyResize(b, h, rows, cols)
+
+  proc ptyForkExec*(b: WindowsBackend, h: string, p: string, a: openArray[string], c: string): int =
+    var size: SIZE_T
+    discard InitializeProcThreadAttributeList(nil, 1, 0, addr size)
+    var attrList = alloc0(size)
+    if InitializeProcThreadAttributeList(attrList, 1, 0, addr size) == 0:
+      raiseWinError(osLastError().int64, "InitializeProcThreadAttributeList")
+    
+    var hPC = b.hPC
+    if UpdateProcThreadAttribute(attrList, 0, PROC_THREAD_ATTRIBUTE_PSEUDOCONSOLE, addr hPC, cast[SIZE_T](sizeof(HPCON)), nil, nil) == 0:
+      raiseWinError(osLastError().int64, "UpdateProcThreadAttribute")
+
+    var si: STARTUPINFOEXW
+    si.StartupInfo.cb = int32(sizeof(STARTUPINFOEXW))
+    si.lpAttributeList = attrList
+
+    var pi: PROCESS_INFORMATION
+    
+    var cmdLine = p
+    for arg in a:
+      cmdLine &= " " & arg
+
+    var wCmdLine = newWideCString(cmdLine)
+    var pCwd: WideCString
+    if c.len > 0: pCwd = newWideCString(c)
+
+    let flags = EXTENDED_STARTUPINFO_PRESENT
+    
+    if createProcessW(nil, wCmdLine, nil, nil, 0, int32(flags), nil, pCwd, si.StartupInfo, pi) == 0:
+      raiseWinError(osLastError().int64, "CreateProcessW")
+
+    DeleteProcThreadAttributeList(attrList)
+    dealloc(attrList)
+
+    b.processHandle = wrap(int(pi.hProcess), closeHandleCloser)
+    b.threadHandle = wrap(int(pi.hThread), closeHandleCloser)
+    b.pid = int(pi.dwProcessId)
+    return b.pid
